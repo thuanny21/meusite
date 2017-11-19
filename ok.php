cf0fd1aea0ccd714b869899414d397ceaf1a150c
Thuanny <thucatm@gmail.com>
2017-11-18 22:22:11 -0200

    Add files via upload

---
+++ ok.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Thuanny Developer</title>
+
+    <!-- Bootstrap -->
+     <meta property="og:type" content="website"/>
+  <meta property="og:title" content="Thuanny Developer - Desenvolvimento de sites"/>
+  <meta property="og:description" content="Desenvolvimento de sites, loja virtual e blog."/>
+  <meta property="og:url" content="http://thuanny.com"/>
+  <meta property="og:image" content="http://thuanny.com/imagem/og.jpg"/>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="owlcarousel/owl.carousel.min.css">
+    <link rel="stylesheet" href="owlcarousel/owl.theme.default.min.css">
+
+  <link rel="sortcut icon" href="imagem/thuanny.gif" type="image/gif"/>
+
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <style>
+      .login-page
+{
+    background-image: url('../imagem/quebra.jpg');
+    background-repeat: no-repeat;
+    background-size:100%;
+    bottom: 0;
+    color: black;
+    left: 0;
+    overflow: auto;
+    padding: 3em;
+    position: absolute;
+    right: 0;
+    text-align: center;
+    top: 0;
+
+}
+.login-page h1
+{
+    font-weight: 300;
+}
+.login-page h1 small
+{
+    color: gray;
+}
+.login-page .form-group
+{
+    padding: 8px 0;
+}
+.login-page .form-content
+{
+    padding: 40px 0;
+}
+    </style>
+  </head>
+
+<body>
+
+<div class=login-page>
+
+   <div class=row>
+      <div class="col-md-4 col-lg-4 col-md-offset-4 col-lg-offset-4">
+        </br>
+        </br>
+        </br>
+        </br>
+        </br>
+        </br>
+        </br>
+        </br>
+         <h1>Sua Mensagem Foi Enviada Com Sucesso!!!</h1>
+         <h2>O mais breve possivel estaremos respondendo sua mensagem</h2>
+
+         <a href="http://thuanny.com" class="btn"> Voltar </a>
+        
+      </div>
+   </div>
+</div>
+</body>
+<html>
+
+
